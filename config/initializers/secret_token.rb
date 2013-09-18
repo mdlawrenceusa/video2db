cd68dd0c79a7ddc4a666f926ebccdaf31e499f98
Michael Lawrence <mdlawrence@me.com>
2013-09-18 16:38:09 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Video2db::Application.config.secret_key_base = '643080ca7b729eb56753c151faa9a162758ee3af8065aac9abb72b3f93a1f4ae506459304e46c44efdc23f92105ddb252d5f707371a6a764132c2a3e0bf31616'
